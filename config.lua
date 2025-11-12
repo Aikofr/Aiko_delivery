abd38b5ed18549be4c1a4071342a238e754fb9de
Aikofr <aikootv@gmail.com>
2025-11-12 17:01:53 +0100

    prep framework & target

--- config.lua
+++ config.lua
@@ -1,5 +1,8 @@
 Config = {}
 
+Config.Framework = 'qbcore' -- 'qbcore', 'esx'
+Config.Target = false       -- 'ox_target', 'qb-target',
+
 Config.BossNPC = {
     model = "a_m_m_farmer_01",
     coords = vector4(-155.76, -1349.81, 29.9, 272.38),
